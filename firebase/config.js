ca88f936fcd3eb2768e5d9afdf774739ea4ec9e8
juankcba <juangblade@hotmail.com>
2021-10-15 15:55:55 -0300

    first commit

--- firebase/config.js
+++ firebase/config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyB-JONvKYwSdY_rFydvlDUQSi5-2ZdIOXQ",
-    authDomain: "product-hunt-1e21e.firebaseapp.com",
-    databaseURL: "https://product-hunt-1e21e.firebaseio.com",
-    projectId: "product-hunt-1e21e",
-    storageBucket: "product-hunt-1e21e.appspot.com",
-    messagingSenderId: "728310011880",
-    appId: "1:728310011880:web:38f7a45b24a8cf7a2a765d"
+    apiKey: "AIzaSyAPRzySkJu3JoK2gIrkzA7VthaTWG3DGXo",
+    authDomain: "product-hunt-9f28d.firebaseapp.com",
+    projectId: "product-hunt-9f28d",
+    storageBucket: "product-hunt-9f28d.appspot.com",
+    messagingSenderId: "613465510067",
+    appId: "1:613465510067:web:b4aedd7570cd713bbe20c6",
+    measurementId: "G-GM1FTR20XZ"
 };
 
 export default firebaseConfig;
